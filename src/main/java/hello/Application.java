dd71afa58254ffb2bd97bed73c70ec484080e988
sathishk <sathishkumar.thiyagarajan@gmail.com>
2015-03-06 08:09:36 +0000

    Spring Boot Changes

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -17,9 +17,13 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan
 public class Application {
 
 	public static void main(String[] args) {
